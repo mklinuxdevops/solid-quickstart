ea67b6dc81039f619974b70f4e66aca5089b4f8c
mklinuxdevops <117293777+mklinuxdevops@users.noreply.github.com>
2023-11-21 17:29:48 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Welcome</Title>
+      <h1>Welcome Dracula!</h1>
       <Counter />
       <p>
         Visit{" "}
