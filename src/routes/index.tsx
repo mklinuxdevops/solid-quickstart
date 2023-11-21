4c8bfe1782eae47fd54473f8c1ef820c683ff9e6
mklinuxdevops <117293777+mklinuxdevops@users.noreply.github.com>
2023-11-21 17:36:03 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,13 +7,6 @@ export default function Home() {
       <Title>Welcome</Title>
       <h1>Welcome Dracula!</h1>
       <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
     </main>
   );
 }
