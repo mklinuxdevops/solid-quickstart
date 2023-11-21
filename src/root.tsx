71ae9178ab62a68001840cca76913dc18b26f998
mklinuxdevops <117293777+mklinuxdevops@users.noreply.github.com>
2023-11-21 18:44:50 +0530

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -27,7 +27,7 @@ export default function Root() {
           <ErrorBoundary>
             <A href="/">Index</A>
             <A href="/about">About</A>
-            <A href="/about">New</A>
+            <A href="/New">New</A>
             <Routes>
               <FileRoutes />
             </Routes>
